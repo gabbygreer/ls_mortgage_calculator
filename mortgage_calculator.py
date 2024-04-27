ad3b90924163a5827ee914106414ce5f3189cd86
Gabby Greer <136518804+gabbygreer@users.noreply.github.com>
2024-04-27 18:34:16 -0500

    Refactored mortgage calculator code. Abstracted functions out of the main while loop. Now when the user chooses to calculate another loan the screen clears.

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 def prompt(message):
     print(f"==> {message}")
 
@@ -5,58 +8,99 @@ def invalid_number(number_str):
     try: 
         number = float(number_str)
         if number <= 0:
-            raise ValueError(f"Value must be > 0: {number_str}")
+            raise ValueError(messages("prompt_invalid_number"))
     except ValueError:
         return True
     return False
 
+def invalid_interest(percent_str):
+    try:
+        percent = float(percent_str)
+        if percent < 0:
+            raise ValueError(messages("prompt_invalid_interest"))
+    except ValueError:
+        return True
+    return False
 
-prompt("Welcome to Mortgage Calculator")
+def messages(message):
+    return MESSAGES[message]
 
-while True:
-    prompt("What's the amount of your loan?")
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
 
-    get_amount = input()
-    while invalid_number(get_amount):
-        prompt("Please enter a positive number")
-        get_amount = input()
+def get_loan_amount():
+    prompt(messages("prompt_loan_amount"))
+    prompt(messages("prompt_loan_amount_example"))
+    amount = input()
 
-    prompt("What's your Annual Percentage Rate (APR)?")
-    prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
+    while invalid_number(amount):
+        prompt(messages("prompt_enter_positive_number"))
+        amount = input()
+
+    return int(amount)
+
+def get_interest_rate():
+    prompt(messages("prompt_interest_rate"))
+    prompt(messages("prompt_interest_rate_example"))
+    interest_rate = input() 
     
-    get_apr = input() 
+    while invalid_interest(interest_rate):
+        prompt(messages("prompt_enter_positive_number"))
+        interest_rate = input()
 
-    while invalid_number(get_apr):
-        prompt("Please enter a positive number")
-        get_apr = input()
+    return float(interest_rate)
 
-    prompt("What's your loan duration (in years)?")
-    get_duration_years = input()
+def get_loan_duration():
+    prompt(messages("prompt_loan_duration"))
+    loan_duration = input()
+    
+    while invalid_number(loan_duration):
+        prompt(messages("prompt_enter_positive_number"))
+        loan_duration = input()
 
-    while invalid_number(get_duration_years):
-        prompt("Please enter a positive number")
-        get_duration_years = input()
+    return float(loan_duration)
 
-    annual_interest_rate = float(get_duration_years) / 100
-    monthly_interest_rate = annual_interest_rate / 12
-    months = float(get_duration_years) * 12
-    loan_amount = float(get_amount)
+def calculate_monthly_payment(amount, interest_rate, loan_duration):
+    
+    interest_rate = (interest_rate / 100) / 12
+    loan_duration_months = loan_duration * 12
+    
+    if interest_rate != 0:
+        return round(amount * (interest_rate / 
+                               (1 - (1 + interest_rate) ** (-loan_duration_months))),
+                               2)
+    return round(loan_amount / (loan_duration_months), 2)
 
-    monthly_payment = loan_amount * (
-        monthly_interest_rate / 
-        (1 - (1 + monthly_interest_rate) ** (-months))
-    )
+def display_result(amount, interest_rate, loan_duration, monthly_payment):
+    prompt(messages("prompt_payment_summary").format(amount=amount,
+                                                     interest_rate=interest_rate,
+                                                     loan_duration=loan_duration,
+                                                     monthly_payment=monthly_payment
+                                                     ))
 
-    prompt(f'Your monthly payment is: ${monthly_payment: .2f}')
-
-    prompt("Do you want to calculate another loan?")
+def new_calculation():
+    prompt(messages("prompt_calculate_another"))
     answer = input().lower()
     while True:
         if answer.startswith('n') or answer.startswith('y'):
             break
 
-        prompt('Please enter "yes" or "no".')
+        prompt(messages("prompt_calculate_another_answer"))
         answer = input().lower()
+    
+    return answer
 
-    if answer[0] == 'n':
+prompt(messages("prompt_welcome_message"))
+
+while True:
+    loan_amount = get_loan_amount()
+    interest_rate = get_interest_rate()
+    loan_duration = get_loan_duration()
+    monthly_payment = calculate_monthly_payment(loan_amount, interest_rate, loan_duration)
+    display_result(loan_amount, interest_rate, loan_duration, monthly_payment)
+
+    answer = new_calculation()
+    if answer == 'y':
+        os.system('clear')
+    else: 
         break
